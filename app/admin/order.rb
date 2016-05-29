c1ef32dd8e1f3af6b8e1ee3f230b36a214adca3e
Prelang Builder <builder@prelang.com>
2016-05-29 03:35:07 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Order' to allow for create, update

--- app/admin/order.rb
+++ app/admin/order.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Order do
+  permit_params :order_no, :date, :amount
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
